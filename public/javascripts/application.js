454ea08b2f8d9aebce72efde34409b4d1d0fe098
Jonathan Chiu <Jonathan@Jonathan-PC.(none)>
2011-01-30 00:40:41 +0800

    Added hovering opaque effect

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+ 
+
+/*
+ $(document).ready(function(){
+ $(".bacon-crisp").mouseover(function () {
+ $(this).css("left", "200px");
+  });
+ 
+});
