db6b43011f2df7b30af9ab147e5c5a60a7164178
Jonathan Chiu <Jonathan@Jonathan-PC.(none)>
2011-01-30 16:50:29 +0800

    Added wooden background

---
+++ public/javascripts/scrolling.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+$('a[href*=#jump]').click(function() {
+if (location.pathname.replace(/^\//,") == this.pathname.replace(/^\//,")
+&& location.hostname == this.hostname) {
+var $target = $(this.hash);
+$target = $target.length && $target
+|| $('[name=' + this.hash.slice(1) +']');
+if ($target.length) {
+var targetOffset = $target.offset().top;
+$('html,body')
+.animate({scrollTop: targetOffset}, 1000);
+return false;
+}
+}
+});
+});
